34ece077ea4cb7a4bd8ee23c83292d8d230fc079
AlexanderDi <67360352+AlexanderDi@users.noreply.github.com>
2023-08-27 23:26:34 +0300

    Add files via upload

---
+++ lesson2_task13.erl
@@ -0,0 +1,16 @@
+-module(lesson2_task13).
+-export([decode_modified/1]).
+-import(lesson2_task05, [reverse/1]).
+
+decode_modified(List) ->
+    decode_modified(List,[]).
+
+decode_modified([],Acc) ->
+    reverse(Acc);    
+decode_modified([{A,B}|T],Acc) ->
+    decode_modified(T, dubl(B,A,Acc)).
+
+dubl(_,0,Acc) ->
+    Acc;
+dubl(B,A,Acc) ->
+    dubl(B,A-1,[B|Acc]).
